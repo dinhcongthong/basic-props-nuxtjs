dc0ce059a7b579f7ef2b01c08f2d4217eaf87140
dinhcongthong <dinhcongthong97y@gmail.com>
2022-09-27 22:45:48 +0700

    first commit

---
+++ components/TestProps.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        This is child component. <br />
+        My name is {{dataFromParents}} <br />
+        <button @click="addToCart()">button</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            dataFromParents: this.user
+        }
+    },
+    props: ['user'],
+    created: function () {
+    },
+    methods: {
+        addToCart (event) {
+            this.$emit('update-cart', 'some value da click roi nhe')
+        }
+    },
+    name: 'TestProps'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TestProps :user="userThong" @update-cart="updateCart2" />
+  </div>
+
+</template>
+
+<script>
+  import TestProps from '../components/TestProps.vue';
+  export default {
+    data (){
+      return {
+        userThong: 'Dinh Cong Thong'
+      }
+    },
+    methods: {
+      updateCart2 (e) {
+        console.log(e);
+      }
+    },
+    name: "IndexPage",
+    components: { TestProps }
+  }
+</script>
